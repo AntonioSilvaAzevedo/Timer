8f8ae50b3fff721387c79883ed59d1ec37e1dd96
Antonio Carlos <antonie.dev@gmail.com>
2022-08-10 15:55:22 -0300

    add hooks-forms validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import {
   CountdownContainer,
   FormContainer,
@@ -9,16 +12,35 @@ import {
   TaskInput,
 } from "./styles";
 
+const newCycleFormValidationScheme = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no máximo 5 minutos")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+});
 export function Home() {
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationScheme),
+  });
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para seu projeto"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -35,7 +57,8 @@ export function Home() {
             placeholder="00"
             step={5}
             min={5}
-            max={60}
+            // max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos</span>
@@ -49,7 +72,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,6 @@
-/* eslint-disable prettier/prettier */
-import styled from "styled-components"
+import styled from "styled-components";
 
-
-export const HomeContainer = styled.main `
+export const HomeContainer = styled.main`
   flex: 1;
 
   display: flex;
@@ -16,76 +14,75 @@ export const HomeContainer = styled.main `
     align-items: center;
     gap: 3.5rem;
   }
-`
-export const FormContainer = styled.main `
+`;
+export const FormContainer = styled.main`
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 0.5rem;
-  color: ${props => props.theme['gray-100']};
+  color: ${(props) => props.theme["gray-100"]};
   font-size: 1.125rem;
   font-weight: bold;
   flex-wrap: wrap;
- 
-`
-export const CountdownContainer = styled.main `
-  font-family: 'Roboto Mono', monospace;
+`;
+export const CountdownContainer = styled.main`
+  font-family: "Roboto Mono", monospace;
   font-size: 10rem;
   line-height: 8rem;
-  color: ${props => props.theme['gray-100']};
+  color: ${(props) => props.theme["gray-100"]};
 
   display: flex;
   gap: 1rem;
 
   span {
-    background: ${props => props.theme['gray-700']};
+    background: ${(props) => props.theme["gray-700"]};
     padding: 2rem 1rem;
     border-radius: 8px;
   }
-`
+`;
 
-export const Separator = styled.div `
+export const Separator = styled.div`
   padding: 2rem 0;
-  color: ${props => props.theme['green-500']};
+  color: ${(props) => props.theme["green-500"]};
 
   width: 4rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
-`
+`;
 
-const BaseInput = styled.input `
+const BaseInput = styled.input`
   background: transparent;
   height: 2.5rem;
   border: 0;
-  border-bottom: 2px solid ${props => props.theme['gray-500']};
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
   font-weight: bold;
   font-size: 1.125rem;
   padding: 0 0.5rem;
-  color: ${props => props.theme['gray-100']};
+  color: ${(props) => props.theme["gray-100"]};
 
   &:focus {
     box-shadow: none;
-    border-color: ${props => props.theme['green-500']};
+    border-color: ${(props) => props.theme["green-500"]};
   }
 
   &::placeholder {
-    color: ${props => props.theme['gray-500']};
+    color: ${(props) => props.theme["gray-500"]};
   }
-`
+`;
 
-export const TaskInput = styled(BaseInput) `
+export const TaskInput = styled(BaseInput)`
   flex: 1;
 
-  &::-webkit-calendar-picker-indicator { //remove flechinho do label list
+  &::-webkit-calendar-picker-indicator {
+    //remove flechinho do label list
     display: none !important;
   }
-
-`
-export const MinutesAmountInput = styled(BaseInput) `
+`;
+export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
-`
+`;
 
 export const StartCountdownButton = styled.button`
   width: 100%;
@@ -102,8 +99,8 @@ export const StartCountdownButton = styled.button`
 
   cursor: pointer;
 
-  background: ${props => props.theme['green-500']};
-  color: ${props => props.theme['gray-100']};
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
 
   &:disabled {
     opacity: 0.7;
@@ -111,9 +108,6 @@ export const StartCountdownButton = styled.button`
   }
 
   &:not(:disabled)hover {
-    background: ${props => props.theme['green-700']};
+    background: ${(props) => props.theme["green-700"]};
   }
-`
-
-
-
+`;
